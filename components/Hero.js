432bf17a42eac3b235c20b901eead0c203e52daa
Tino_taylor <tino8music@gmail.com>
2021-10-14 21:02:24 +0200

    Hero mobile adjustments for mobile

--- components/Hero.js
+++ components/Hero.js
@@ -21,37 +21,38 @@ function Hero(props) {
   }
 
   return (
-    <section className="section my-container ">
-      <div className="relative">
-        <div className="flex justify-center place-items-center w-full">
+    <section className="section  h-screen my-container ">
+      <div className="relative flex flex-col w-full h-full">
+        <div className="flex justify-center place-items-center w-full flex-1 relative">
+      
           <img
-            className="heroImageDimensions relative left-[9.5em] shadow-2xl object-cover pointer-events-none"
+            className=" heroImageDimensions relative left-[48%] xs:left-[9.5em] md:left-[15em] lg:left-[9.5em] shadow-2xl object-cover pointer-events-none"
             src="/images/KNSTNTIN.jpg"
             alt="Hero image"
           />
           <img
-            className="heroImageDimensions relative right-[9.5em] shadow-2xl object-cover pointer-events-none"
+            className=" heroImageDimensions relative right-[48%] xs:right-[9.5em] md:right-[15em] lg:right-[9.5em] shadow-2xl object-cover pointer-events-none"
             src="/images/KNSTNTIN.jpg"
             alt="Hero image"
           />
           <img
-            className="absolute heroImageDimensions shadow-xl object-cover pointer-events-none"
+            className="absolute heroImageDimensions shadow-xl object-cover cursor-pointer transition-all duration-700 ease-out hover:animate-pulse"
             src="/images/KNSTNTIN.jpg"
             alt="Hero image"
           />
-          <h1 className="absolute ml-3 font-Bulgari text-center tracking-[.110em] leading-[130px] max-w-[790px] text-white mix-blend-difference text-[20vw] sm:text-[150px] md:text-[125px] pointer-events-none">
+          <h1 className="absolute ml-3 font-Bulgari text-center tracking-[.110em] leading-[130px] max-w-[790px] text-white mix-blend-difference text-[15vw]  md:text-[120px] lg:text-[140px] xl:text-[150px] pointer-events-none">
             CREATIVE DEVELOPER
           </h1>
         </div>
 
-        <h2 className="absolute uppercase left-2 top-1/2  text-[15px] font-bold tracking-[.100em] transform  origin-left -rotate-90">
+        <h2 className="hidden absolute uppercase left-2 top-1/2  text-[15px] font-bold tracking-[.100em] transform  origin-left -rotate-90 xs:block">
           LOVE FOR CREATING
         </h2>
-        <h2 className="absolute uppercase right-2 top-1/2 text-[15px] font-bold tracking-[.100em] transform origin-right rotate-90">
+        <h2 className="hidden absolute uppercase right-2 top-1/2 text-[15px] font-bold tracking-[.100em] transform origin-right rotate-90 xs:block">
           LOVE FOR CREATING
         </h2>
 
-        <div className="flex uppercase justify-between text-[15px] mt-[86px] font-bold tracking-[.100em]">
+        <div className="relative bottom-0 flex uppercase justify-between text-[15px] mb-10 mt-[86px] font-bold tracking-[.100em]">
           <h2>SCROLL MORE</h2>
           <ChevronDownIcon className="w-8 animate-bounce opacity-[14%] cursor-pointer" />
           <h2>LDN / NRW</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,25 @@ module.exports = {
         HelveticaNeue: ["Helvetica Neue", "sans-serif"],
       },
     },
+    screens: {
+      xs: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   pseudo: {
     // defaults to {'before': 'before', 'after': 'after'}
@@ -31,6 +50,5 @@ module.exports = {
     require("tailwindcss-pseudo")({
       empty: true, // defaults to true
     }),
-
   ],
 };
