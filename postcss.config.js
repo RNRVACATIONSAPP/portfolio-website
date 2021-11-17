3221ae664a2299b9fb56a927c87ac230c76c790d
Tino_taylor <tino8music@gmail.com>
2021-11-17 17:09:44 +0100

    trying a new solution for sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,18 @@
 // postcss.config.js
 module.exports = {
 	plugins: {
-	  tailwindcss: {},
-	  autoprefixer: {},
-	},
-  }
+		'postcss-import': {},
+		autoprefixer: {},
+		tailwindcss: {},
+		'postcss-flexbugs-fixes': {},
+		'postcss-preset-env': {
+			autoprefixer: {
+				flexbox: 'no-2009'
+			},
+			stage: 3,
+			features: {
+				'custom-properties': false
+			}
+		}
+	}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = {
   plugins: [
     require("@tailwindcss/forms"),
     require("tailwindcss"),
-
+    require("precss"),
+    require("autoprefixer"),
   ],
 };
